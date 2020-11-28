4ecf70218a177364ada969d6caddd79c9863447e
Allah-The-Dev <pandeyneeleshkumar@gmail.com>
2020-11-29 04:59:07 +0530

    basic home page designed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import Header from './Header';
+import './App.css';
 
 function App() {
+
+  const subscriber = [
+    {
+      id: 1,
+      name: 'Neelesh',
+      phone: 858585
+    },
+    {
+      id: 2,
+      name: 'Aniket',
+      phone: 858585
+    },
+    {
+      id: 2,
+      name: 'Mukesh',
+      phone: 969696
+    }
+  ];
 
   return (
     <div>
       <Header/>
-      <button>Add</button>
-      <div>
-          <span>Name</span><br />
-          <span>Phone</span>
+
+      <div className="component-body-container">
+        <button className="custom-btn add-btn">Add</button>
+
+        <div className="grid-container heading-container">
+          <span className="grid-item name-heading">Name</span>
+          <span className="grid-item phone-heading">Phone</span>
         </div>
+
+        {
+          subscriber.map(subs => {
+            return <div key={subs.id} className="grid-container">
+              <span className="grid-item">{subs.name}</span>
+              <span className="grid-item">{subs.phone}</span>
+              <button className="deleteBtn">Delete</button>
+            </div>
+          })
+        }
+    
+      </div>
     </div>
   );
 }
